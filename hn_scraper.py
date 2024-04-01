bad99608e92ad5e3aaf92cd74a232643d300182f
hjung01 <hanseul.jung1@gmail.com>
2024-04-02 00:23:13 +1300

    a

---
+++ hn_scraper.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
+# r = requests.get(rf'https://www.harveynorman.co.nz/whiteware/refrigeration/?', headers=HEADERS)
+# soup = BeautifulSoup(r.content, "html.parser")
+# print(soup)
+# p = soup.find_all('span', {'class': 'price-num'})
+# print(len(p))
+
+with requests.Session() as session:
+    session.headers.update({
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+    })
+
+    response = session.get('https://www.harveynorman.co.nz/whiteware/refrigeration/?', headers=HEADERS)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    print(soup)
+    prices = soup.find_all('span', {'class': 'price-num'})
+    print(response.url)
+    print(len(prices))
--- scrape01/scrape01/spiders/hn_scraper.py
+++ scrape01/scrape01/spiders/hn_scraper.py
@@ -7,6 +7,9 @@ import csv
 
 class hnSpider(scrapy.Spider):
     name = "harveys"
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    }
 
     def start_requests(self):
         urls = [
@@ -16,5 +19,6 @@ class hnSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        p_info_json  = response.xpath('//span[@class="price-num"]').get()
+        print(response.text)
+        p_info_json  = response.xpath('//span[@class="price-num"]').getall()
         print(p_info_json)
