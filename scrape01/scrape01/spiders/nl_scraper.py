8548181efc5c704bfe9ff9df5a8cf2caf912a243
hjung01 <hanseul.jung1@gmail.com>
2024-04-02 21:39:02 +1300

    loops through pages of a category page to get all prod info

--- scrape01/scrape01/spiders/nl_scraper.py
+++ scrape01/scrape01/spiders/nl_scraper.py
@@ -1,34 +1,38 @@
 from pathlib import Path
 
 import scrapy
+from scrapy.spiders import Spider
 import json
 import csv
+import os
 
 
 class noelsSpider(scrapy.Spider):
     name = "noels"
+    start_urls = [f'https://www.noelleeming.co.nz/c/appliances?start=0']
+    filename = 'nl_prod_info.csv'
 
-    def start_requests(self):
-        urls = [
-            f"https://www.noelleeming.co.nz/c/appliances",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    def __init__(self, *args, **kwargs):
+        super(noelsSpider, self).__init__(*args, **kwargs)
+        if Path(self.filename).exists():
+            os.remove(self.filename)
 
     def parse(self, response):
         p_info_json  = response.xpath('//div[@class="product-tile"]/@data-gtm-product').getall()
         products = [json.loads(product) for product in p_info_json]
         filename = 'nl_prod_info.csv'
 
-        with Path(filename).open('w', newline='', encoding='utf-8') as csvfile:
-            if products:
-                fieldnames = products[0].keys()
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
+        with Path(filename).open('a', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=products[0].keys() if products else [])
+            if csvfile.tell() == 0 and products:
                 writer.writeheader()
-
-                for product in products:
-                    writer.writerow(product)
+            for product in products:
+                writer.writerow(product)
         
-        self.log(f'Saved file {filename}')
+        self.log(f'Appended to file {filename}')
         
+        if products:
+            current_start = int(response.url.split('start=')[-1])
+            next_start = current_start + 32
+            next_page_url = f"https://www.noelleeming.co.nz/c/appliances?start={next_start}"
+            yield response.follow(next_page_url, self.parse)
