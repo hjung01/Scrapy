e29be79c02be9c0146ffd083f4f283246fc176d5
hjung01 <hanseul.jung1@gmail.com>
2024-04-03 00:53:58 +1300

    comments added to nl_scraper.py

--- scrape01/scrape01/spiders/nl_scraper.py
+++ scrape01/scrape01/spiders/nl_scraper.py
@@ -8,31 +8,31 @@ import os
 
 
 class noelsSpider(scrapy.Spider):
-    name = "noels"
-    start_urls = [f'https://www.noelleeming.co.nz/c/appliances?start=0']
-    filename = 'nl_prod_info.csv'
+    name = "noels" #name of spider
+    start_urls = [f'https://www.noelleeming.co.nz/c/appliances?start=0'] #initial url to start scraping
+    filename = 'nl_prod_info.csv' #filename for csv output of data
 
     def __init__(self, *args, **kwargs):
         super(noelsSpider, self).__init__(*args, **kwargs)
-        if Path(self.filename).exists():
-            os.remove(self.filename)
+        if Path(self.filename).exists(): #checks if csv file already exists
+            os.remove(self.filename) # if file exists, delete the file
 
     def parse(self, response):
-        p_info_json  = response.xpath('//div[@class="product-tile"]/@data-gtm-product').getall()
-        products = [json.loads(product) for product in p_info_json]
-        filename = 'nl_prod_info.csv'
+        p_info_json  = response.xpath('//div[@class="product-tile"]/@data-gtm-product').getall() #xpath for product info html
+        products = [json.loads(product) for product in p_info_json] #parse each JSON string into python dictionary
+        filename = 'nl_prod_info.csv' #file to write data to
 
         with Path(filename).open('a', newline='', encoding='utf-8') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=products[0].keys() if products else [])
-            if csvfile.tell() == 0 and products:
-                writer.writeheader()
-            for product in products:
-                writer.writerow(product)
+            if csvfile.tell() == 0 and products: #if file is empty and there is product data
+                writer.writeheader() #write header row for csv
+            for product in products: #for each product in the dictionary
+                writer.writerow(product) #write product data in the row
         
-        self.log(f'Appended to file {filename}')
+        self.log(f'Appended to file {filename}') #logs message that data was successfully appended
         
-        if products:
-            current_start = int(response.url.split('start=')[-1])
-            next_start = current_start + 32
-            next_page_url = f"https://www.noelleeming.co.nz/c/appliances?start={next_start}"
-            yield response.follow(next_page_url, self.parse)
+        if products: #if there was any product information on the page
+            current_start = int(response.url.split('start=')[-1]) #extracts current 'start' parameter from page url for pagination
+            next_start = current_start + 32 #32 products on each page
+            next_page_url = f"https://www.noelleeming.co.nz/c/appliances?start={next_start}" #constructs new url to scrape +=32
+            yield response.follow(next_page_url, self.parse) #schedules request to next page, calling parse method and looping
