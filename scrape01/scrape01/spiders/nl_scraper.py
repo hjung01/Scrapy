80431325345c5c9dd1728f889d02350531db3e64
hjung01 <hanseul.jung1@gmail.com>
2024-04-16 00:58:07 +1200

    sqlite added

--- scrape01/scrape01/spiders/nl_scraper.py
+++ scrape01/scrape01/spiders/nl_scraper.py
@@ -6,14 +6,14 @@ import csv
 import os
 import time
 import logging
-from scrapy.utils.log import configure_logging 
+from scrapy.utils.log import configure_logging
+from datetime import datetime
+import sqlite3
 
 
 class noelsSpider(scrapy.Spider):
     name = "noels" #name of spider
-    # custom_settings = {
-    #     'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
-    # } # custom user agent
+    time.sleep(10)
     start_urls = [f'https://www.noelleeming.co.nz/search?q=&start=0'] #initial url to start scraping
     filename = 'nl_prod_info.csv' #filename for csv output of data
 
@@ -26,28 +26,45 @@ class noelsSpider(scrapy.Spider):
         level=logging.INFO
     )
 
+
     def __init__(self, *args, **kwargs):
         super(noelsSpider, self).__init__(*args, **kwargs)
-        time.sleep(5)
-        if Path(self.filename).exists(): #checks if csv file already exists
-            os.remove(self.filename) # if file exists, delete the file
+        self.setup_database()
+
 
     def parse(self, response):
         p_info_json  = response.xpath('//div[@class="product-tile"]/@data-gtm-product').getall() #xpath for product info html
-        products = [json.loads(product) for product in p_info_json] #parse each JSON string into python dictionaryf
-        filename = 'nl_prod_info.csv' #file to write data to
+        products = [json.loads(product) for product in p_info_json] #parse each JSON string into python dictionary
+        today = datetime.now().date()
 
-        with Path(filename).open('a', newline='', encoding='utf-8') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=products[0].keys() if products else [])
-            if csvfile.tell() == 0 and products: #if file is empty and there is product data
-                writer.writeheader() #write header row for csv
-            for product in products: #for each product in the dictionary
-                writer.writerow(product) #write product data in the row
-        
-        self.log(f'Appended to file {filename}') #logs message that data was successfully appended
+        for product in products:
+            product['date_collected'] = today
+        # Assuming 'product' is a dictionary with keys 'name', 'price', 'other_details'
+            self.cursor.execute('''
+                INSERT INTO NL_product_data (name, price, other_details) VALUES (?, ?, ?)
+            ''', (product['name'], product['price'], product.get('other_details')))
+        self.conn.commit()
         
         if products: #if there was any product information on the page
             current_start = int(response.url.split('start=')[-1]) #extracts current 'start' parameter from page url for pagination
             next_start = current_start + 32 #32 products on each page
             next_page_url = f"https://www.noelleeming.co.nz/search?q=&start={next_start}" #constructs new url to scrape +=32
             yield response.follow(next_page_url, self.parse) #schedules request to next page, calling parse method and looping
+
+
+    def setup_database(self):
+        self.conn = sqlite3.connect(f'D:\SQLite DB\scrape_data.db')  # The database file
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS NL_product_data (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                price TEXT,
+                other_details TEXT
+            )
+        ''')
+        self.conn.commit()
+
+
+    def closed(self, reason):
+        self.conn.close()
